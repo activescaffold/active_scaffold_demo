45618fb6f712c01206c3c3740a7927a3d3edc7ad
edwin.moss <edwin.moss@561dde7e-7729-0410-be8e-ef83869d6c7d>
2007-03-09 19:02:25 +0000

    Demo, show source next to scaffold.
    git-svn-id: http://activescaffold.googlecode.com/svn/applications/demo@112 561dde7e-7729-0410-be8e-ef83869d6c7d

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,14 @@
 # Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
+  def show_code(path, filename, comment = "")
+    file = File.open(Dir.pwd + "/../app/#{path}/#{filename}")
+<<PRE_BLOCK
+<div class="Heading_1">/#{path}/#{filename} #{comment}</div>
+<pre>
+#{file.read.gsub("<", "&lt;").gsub(">", "&gt;")}
+</pre>
+<div class="Body_1">&nbsp;</div>
+PRE_BLOCK
+  end
 
 end
