c53ecd127cd90e10d0f4760b8544f6dfb6148f7c
edwin.moss <edwin.moss@561dde7e-7729-0410-be8e-ef83869d6c7d>
2007-03-12 23:14:22 +0000

    Demo, cleanup.
    git-svn-id: http://activescaffold.googlecode.com/svn/applications/demo@137 561dde7e-7729-0410-be8e-ef83869d6c7d

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,16 +5,13 @@ class UsersController < DemoController
   end
 
   active_scaffold :user do |config|
-    config.actions << :field_search
-    config.field_search.link.label = "Search By Field"
-
     config.columns = [:id, :name, :first_name, :middle_name, :last_name, :phone_number, :email_address, :login, :password, :location, :groups, :roles, :aliases]
     config.columns[:location].ui_type = :select
     config.columns[:phone_number].description = "(Format: ###-###-####)"
     config.columns[:phone_number].label = "Phone"
     
     config.create.columns.exclude :id, :name
-    config.list.columns.exclude :first_name, :middle_name, :last_name, :aliases
+    config.list.columns.exclude :first_name, :middle_name, :last_name
     config.list.sorting = {:login => 'ASC'}
     config.update.columns.exclude :id, :name
     
