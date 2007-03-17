e34f8ed80cb37984267085ba7a38a54c3d9939bf
edwin.moss <edwin.moss@561dde7e-7729-0410-be8e-ef83869d6c7d>
2007-03-17 19:10:36 +0000

    Demo - Alphas are now Forests...
    Make sure you drop your DB!
    
    git-svn-id: http://activescaffold.googlecode.com/svn/applications/demo@203 561dde7e-7729-0410-be8e-ef83869d6c7d

--- db/migrate/001_initial_schema.rb
+++ db/migrate/001_initial_schema.rb
@@ -1,52 +1,60 @@
 class InitialSchema < ActiveRecord::Migration
   def self.up
-    create_table :alphas, :force => true do |t|
+    # Was alphas
+    create_table :forests, :force => true do |t|
       t.column :name, :string
+      t.column :meta_title, :string
+      t.column :meta_keyword, :string
       t.column :created_on, :datetime
       t.column :updated_on, :datetime
     end
 
-    create_table :betas, :force => true do |t|
+    # Was betas
+    create_table :rocks, :force => true do |t|
       t.column :name, :string, :null => false
-      t.column :alpha_id, :integer
+      t.column :forest_id, :integer, :null => false
       t.column :created_at, :datetime
       t.column :updated_at, :datetime
     end
 
-    create_table :gammas, :force => true do |t|
+    # Was gammas
+    create_table :trees, :force => true do |t|
       t.column :name, :string, :null => false
-      t.column :alpha_id, :integer
+      t.column :forest_id, :integer, :null => false
       t.column :created_on, :datetime
       t.column :updated_on, :datetime
     end
 
-    create_table :deltas, :force => true do |t|
+    # Was deltas
+    create_table :lakes, :force => true do |t|
       t.column :title, :string, :null => false
       t.column :created_on, :datetime
       t.column :updated_on, :datetime
     end
 
-    create_table :alphas_deltas, :id => false, :force => true do |t|
-      t.column :alpha_id, :integer
-      t.column :delta_id, :integer
+    create_table :forests_lakes, :id => false, :force => true do |t|
+      t.column :forest_id, :integer, :null => false
+      t.column :lake_id, :integer, :null => false
     end
 
-    create_table :epsilons, :force => true do |t|
+    # Was epsilons
+    create_table :trunks, :force => true do |t|
       t.column :name, :string, :null => false
-      t.column :gamma_id, :integer
+      t.column :tree_id, :integer, :null => false
       t.column :created_on, :datetime
       t.column :updated_on, :datetime
     end
 
-    create_table :zetas, :force => true do |t|
+    # Was zetas
+    create_table :roots, :force => true do |t|
       t.column :name, :string, :null => false
-      t.column :gamma_id, :integer
+      t.column :tree_id, :integer, :null => false
       t.column :created_on, :datetime
       t.column :updated_on, :datetime
     end
   end
 
   def self.down
-    %w(alphas betas gammas deltas alphas_deltas epsilons zetas).each { |table| drop_table(table) }
+    %w(forests rocks trees lakes forests_lakes trunks roots).each { |table| drop_table(table) }
   end
 end
