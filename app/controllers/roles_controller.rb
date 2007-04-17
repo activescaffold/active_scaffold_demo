895c3572b537d6c5754aa88989c7321ce23ecd89
edwin.moss <edwin.moss@561dde7e-7729-0410-be8e-ef83869d6c7d>
2007-04-17 19:10:34 +0000

    Cleanup environment.rb
    Add a field search example to roles.
    
    git-svn-id: http://activescaffold.googlecode.com/svn/applications/demo@382 561dde7e-7729-0410-be8e-ef83869d6c7d

--- app/controllers/roles_controller.rb
+++ app/controllers/roles_controller.rb
@@ -2,6 +2,8 @@ class RolesController < DemoController
 
   active_scaffold :roles do |config|
     config.theme = :blue
+    config.actions.exclude :search
+    config.actions << :field_search
   end
 
 end
--- config/environment.rb
+++ config/environment.rb
@@ -1,7 +1,4 @@
 # ENV['RAILS_ENV'] ||= 'production'
-# ENV['GEM_PATH'] = '/usr/lib/ruby/gems/1.8:/home/fourthefamily/.gems'
-
-#RAILS_GEM_VERSION = '1.1.6'
 RAILS_GEM_VERSION = '1.2'
 
 require File.join(File.dirname(__FILE__), 'boot')
