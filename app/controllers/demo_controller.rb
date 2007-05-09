2d8d1540bdcecf35c27ae7146b78ca54aaedee93
rrwhite <rrwhite@561dde7e-7729-0410-be8e-ef83869d6c7d>
2007-05-09 05:46:53 +0000

    * Fixing tabs on demo application
    git-svn-id: http://activescaffold.googlecode.com/svn/applications/demo@451 561dde7e-7729-0410-be8e-ef83869d6c7d

--- app/controllers/demo_controller.rb
+++ app/controllers/demo_controller.rb
@@ -2,12 +2,4 @@ class DemoController < ApplicationController
   def index
     redirect_to :controller => 'users'
   end
-  
-  def delete_all
-    Location.destroy_all
-    Permission.destroy_all
-    Role.destroy_all
-    User.destroy_all
-    index
-  end
 end
