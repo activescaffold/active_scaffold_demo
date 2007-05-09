4cdde5cfaf8ba2f0556df79189b93bd93c23aef6
rrwhite <rrwhite@561dde7e-7729-0410-be8e-ef83869d6c7d>
2007-05-09 05:59:34 +0000

    * Resized the Demo to match activescaffold.com * Added a link to download the demo from SVN
    git-svn-id: http://activescaffold.googlecode.com/svn/applications/demo@452 561dde7e-7729-0410-be8e-ef83869d6c7d

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,14 +1,15 @@
 class UsersController < DemoController
 
   active_scaffold :user do |config|
-    config.columns = [:id, :name, :first_name, :middle_name, :last_name, :phone_number, :email_address, :login, :password, :roles, :aliases, :addresses]
+    config.columns = [:id, :name, :first_name, :middle_name, :last_name, :phone_number, :email_address,
+                      :login, :password, :roles, :aliases, :addresses]
 
     config.columns[:phone_number].description = "(Format: ###-###-####)"
     config.columns[:phone_number].label = "Phone"
 
     config.create.columns.exclude :id, :name
     config.update.columns.exclude :id, :name
-    config.list.columns.exclude :first_name, :middle_name, :last_name, :password
+    config.list.columns.exclude :first_name, :middle_name, :last_name, :password, :phone_number
     config.subform.columns = [:first_name, :last_name, :login, :password]
 
     config.list.sorting = {:login => 'ASC'}
