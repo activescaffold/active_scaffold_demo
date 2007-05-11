e1d0a3dcd01fa188deaa884a32c2c2413d73ce97
rrwhite <rrwhite@561dde7e-7729-0410-be8e-ef83869d6c7d>
2007-05-11 05:28:56 +0000

    * Added to_label for Address
    git-svn-id: http://activescaffold.googlecode.com/svn/applications/demo@466 561dde7e-7729-0410-be8e-ef83869d6c7d

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,3 +1,8 @@
 class Address < ActiveRecord::Base
   belongs_to :addressable, :polymorphic => true
+
+  def to_label
+    [[self.city, self.state].compact.join(' '), self.zip].compact.join(', ')
+  end
+
 end
