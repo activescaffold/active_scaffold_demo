9f14b1734aa8e114107de1dcbea53f077cac3bf1
cainlevy <cainlevy@561dde7e-7729-0410-be8e-ef83869d6c7d>
2007-05-24 14:47:16 +0000

    fixed secured_records_helper#owner_column to be aware of record.owner.nil?
    git-svn-id: http://activescaffold.googlecode.com/svn/applications/demo@499 561dde7e-7729-0410-be8e-ef83869d6c7d

--- app/helpers/secured_records_helper.rb
+++ app/helpers/secured_records_helper.rb
@@ -1,5 +1,6 @@
 module SecuredRecordsHelper
   def owner_column(record)
+    return '' unless record.owner
     content = record.owner.to_label
     content = "<span class='restricted'>#{content}</span>" unless current_login and record.owner == current_login
     content
