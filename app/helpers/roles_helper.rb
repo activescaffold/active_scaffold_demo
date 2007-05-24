d90ed43b19ef3569b3e0f72d7168dbf0756952a7
cainlevy <cainlevy@561dde7e-7729-0410-be8e-ef83869d6c7d>
2007-05-24 23:47:12 +0000

    adding field and form overrides to the demo
    git-svn-id: http://activescaffold.googlecode.com/svn/applications/demo@502 561dde7e-7729-0410-be8e-ef83869d6c7d

--- app/helpers/roles_helper.rb
+++ app/helpers/roles_helper.rb
@@ -1,3 +1,11 @@
 module RolesHelper
   include DemoHelper
+
+  def omnipotent_column(record)
+    record.omnipotent? ? 'yes' : 'no'
+  end
+
+  def omnipotent_form_column(record, name)
+    check_box :record, :omnipotent, :name => name
+  end
 end
