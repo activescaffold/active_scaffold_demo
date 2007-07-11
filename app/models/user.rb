7b54dbacb6c5081da54db65a196b8097e31a62ab
timcharper <timcharper@561dde7e-7729-0410-be8e-ef83869d6c7d>
2007-07-11 05:46:51 +0000

    added file uploads demo (need rMagick, FileColumnBridge and FileColumn)
    git-svn-id: http://activescaffold.googlecode.com/svn/applications/demo@570 561dde7e-7729-0410-be8e-ef83869d6c7d

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,12 @@ class User < ActiveRecord::Base
   has_many :addresses, :as => :addressable
 
   validates_presence_of :first_name, :last_name
-
+  file_column :photo, :magick => { 
+    :versions => {
+      :thumb => {:size => "40x40"} 
+    } 
+  }
+  
   def name
     last_name.nil? ? first_name : first_name + " " + last_name
   end
