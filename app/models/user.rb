01ac5f1284d65d9d60f08b7d840143a9e91de188
edwin.moss <edwin.moss@561dde7e-7729-0410-be8e-ef83869d6c7d>
2007-07-24 14:58:23 +0000

    Include fille_column and bridge in externals. Comment out the :magick hash so the RMagick is an optional dependency.
    git-svn-id: http://activescaffold.googlecode.com/svn/applications/demo@579 561dde7e-7729-0410-be8e-ef83869d6c7d

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,11 +4,11 @@ class User < ActiveRecord::Base
   has_many :addresses, :as => :addressable
 
   validates_presence_of :first_name, :last_name
-  file_column :photo, :magick => { 
-    :versions => {
-      :thumb => {:size => "40x40"} 
-    } 
-  }
+  file_column :photo#, :magick => { 
+  #   :versions => {
+  #     :thumb => {:size => "40x40"} 
+  #   } 
+  # }
   
   def name
     last_name.nil? ? first_name : first_name + " " + last_name
