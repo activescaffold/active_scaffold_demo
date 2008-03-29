3054c3ace3d38e45ec848d7f037df6a2d66381ab
edwin.moss <edwin.moss@561dde7e-7729-0410-be8e-ef83869d6c7d>
2008-03-29 22:37:51 +0000

    Not yet ready for this big step to Rails 2.0.
    git-svn-id: http://activescaffold.googlecode.com/svn/applications/demo@739 561dde7e-7729-0410-be8e-ef83869d6c7d

--- config/environment.rb
+++ config/environment.rb
@@ -1,15 +1,11 @@
 # ENV['RAILS_ENV'] ||= 'production'
-RAILS_GEM_VERSION = '2' unless defined? RAILS_GEM_VERSION
+RAILS_GEM_VERSION = '1.2'
 
 require File.join(File.dirname(__FILE__), 'boot')
 
 Rails::Initializer.run do |config|
-  config.action_controller.session = {
-    :session_key => '_ASDemo_session',
-    :secret      => 'c2d0f3ae9f3f997dfe368a738d0b04cfff6f00bcca4b9d5a1d464eca19ddd564936c8990e36a9f7a437db4bf2ee64068019f4a5c978c4d82e1fe618d58c8becd'
-  }
 end
 
 ActiveScaffold.set_defaults do |config| 
-  config.ignore_columns.add [:created_at, :updated_at]
+  config.ignore_columns.add [:created_at, :updated_at, :id]
 end
