6c0ab59278c1d85d206763120c6c89e3a68eb31c
Mike Gaffney <mike@uberu.com>
2009-01-21 23:11:26 -0600

    Revert "removing requirement for now"
    This reverts commit 1b66bdec303e2c5e515fccd609100118c5b86967.
    
    Conflicts:
    
    	config/environment.rb

--- config/environment.rb
+++ config/environment.rb
@@ -6,9 +6,13 @@ require File.join(File.dirname(__FILE__), 'boot')
 Rails::Initializer.run do |config|
   config.action_controller.session = { :session_key => "_active_scaffold_demo", :secret => ("shhh." * 6) }
 <<<<<<< HEAD:config/environment.rb
+<<<<<<< HEAD:config/environment.rb
 #  config.gem 'brynary-webrat', :lib => false, :source => 'http://gems.github.com'
 =======
 >>>>>>> 49aada3... working on getting edge up to date:config/environment.rb
+=======
+  config.gem 'brynary-webrat', :lib => false, :source => 'http://gems.github.com'
+>>>>>>> 1b66bde... removing requirement for now:config/environment.rb
 end
 
 ActiveScaffold.set_defaults do |config| 
