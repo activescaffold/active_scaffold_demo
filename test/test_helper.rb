49aada30925606b92c1a014b5312d9da3e46c824
Mike Gaffney <mike@uberu.com>
2009-01-21 22:26:35 -0600

    working on getting edge up to date

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,6 +1,6 @@
 require 'test/unit'
 
-require File.expand_path(File.join(File.dirname(__FILE__), '../../../../config/environment.rb'))
+require File.expand_path(File.join(File.dirname(__FILE__), '../config/environment.rb'))
 
 for file in %w[model_stub const_mocker]
   require File.join(File.dirname(__FILE__), file)
