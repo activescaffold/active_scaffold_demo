bd6ca09c29df89bd5e3c3ce9fae5595f8f01dc14
Mike Gaffney <mike@uberu.com>
2009-01-24 12:44:16 -0600

    working on edit tests

--- test/factories.rb
+++ test/factories.rb
@@ -1,3 +1,7 @@
 Factory.sequence :name do |n|
   "name #{n}"
 end
+
+Factory.define :tracking_device do |t|
+  t.name Factory.next(:name)
+end
--- test/integration/nature/tracking_devices_integration_test.rb
+++ test/integration/nature/tracking_devices_integration_test.rb
@@ -30,18 +30,47 @@ class Nature::TrackingDevicesIntegrationTest < ActionController::IntegrationTest
       end
       
     end
+    
+    context "sorting" do 
+      should "allow triple sort on name" do
+        click_link "Name"
+        click_link "Name"
+        click_link "Name"
+      end
+      should "allow triple sort on taggable" do
+        click_link "Taggable"
+        click_link "Taggable"
+        click_link "Taggable"
+      end
+    end
+    
+    context "edit" do 
+      setup do
+        TrackingDevice.all.each(&:delete)
+        @editable = Factory(:tracking_device)
+        click_link "nature__tracking_devices-edit-#{@editable.id}-link"
+      end
+      
+      should "allow cancel" do
+        click_link "Cancel"
+      end
+      
+      should "allow close" do
+        click_link "Close"
+      end
+      
+      should "allow editing" do
+        fill_in("Name", :with => "different_name")
+        click_button "Update"
+        sleep 0.1
+        @editable.reload
+        assert_equal("different_name", @editable.name)
+      end
+      
+    end
   end
   
-  # create cancel
-  # create X
-  # create empty
-  # create create
-  # sort name
-  # sort taggable
-  # edit
-  #   invalid
-  #   cancel
-  #   X
+  
   # delete
   # show
   # search
