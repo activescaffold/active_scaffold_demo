d309ad7a34d62ab1bd673c8b54c4988af796b8ce
Mike Gaffney <mike@uberu.com>
2009-01-24 00:02:04 -0600

    added more navigation tests

--- test/integration/base_navigation_integration_test.rb
+++ test/integration/base_navigation_integration_test.rb
@@ -11,7 +11,16 @@ class BaseNavigationIntegrationTest < ActionController::IntegrationTest
   
     context "scenarios" do
       ["habtm", "has_many", "belongs_to", "has_one", "join model", "polymorphic", "security", "field/form overrides"].each do |scenario|
-        should "be able to jump to #{scenario}" do
+        should "be able to jump to scenario #{scenario}" do
+          select scenario
+        end
+      end
+    end
+    
+    context "controllers" do
+      ["account", "addresses", "alias", "demo", "bears", "lakes", "parks", 
+       "tracking_devices", "permissions", "roles", "secured_records", "users"].each do |scenario|
+        should "be able to jump to controller #{scenario}" do
           select scenario
         end
       end
