d28b84d0a59e47ce137c8967fcfc0731fb8c01a3
Kenny Ortmann <kenny.ortmann@gmail.com>
2009-05-07 14:11:41 -0700

    added example of overriding format to bear controller, update AS

--- app/controllers/nature/bears_controller.rb
+++ app/controllers/nature/bears_controller.rb
@@ -1,6 +1,12 @@
 class Nature::BearsController < ApplicationController
   active_scaffold do |c|
     c.columns[:ticks].calculate = :avg
-    c.show.formats << :pdf
+    c.formats = [:txt]
+    c.list.columns << :txt
   end
+
+  def show_respond_to_txt
+    render :text => @record.name
+  end
+
 end
--- app/helpers/nature/bears_helper.rb
+++ app/helpers/nature/bears_helper.rb
@@ -1,2 +1,5 @@
 module Nature::BearsHelper
+  def txt_column record
+    link_to 'txt', nature_bear_path(record, :format => 'txt'), :popup => true
+  end
 end
